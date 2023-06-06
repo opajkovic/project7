71cc2b834d15a3623ba706600d0afc2048944a80
opajkovic <oljaolja170@gmail.com>
2023-06-06 22:38:33 +0200

    animated headerCartButton

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cartContext';
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [btnAnimated, setBtnAnimated] = useState(false);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((curNum, item) => { return curNum + item.amount }, 0);
+  const numberOfCartItems = items.reduce((curNum, item) => { return curNum + item.amount }, 0);
+
+  useEffect(() => {
+    if (items.length === 0) { return; }
+    setBtnAnimated(true);
+
+    const timer = setTimeout(() => { setBtnAnimated(false) }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+
+  const btnClasses = `${classes.button} ${btnAnimated ? classes.bump : ''} `;
 
   return (
-    <button className={classes.button} onClick={props.onClick} >
+    <button className={btnClasses} onClick={props.onClick} >
       <span className={classes.icon}><CartIcon /></span>
       <span>Your Cart</span>
       <span className={classes.badge}>{numberOfCartItems}</span>
